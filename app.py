684d863e0233eac922348d6d6f64f0beacad6b88
Katie Ortiz <katrina.ortiz.673@my.csun.edu>
2023-11-13 22:36:00 -0800

    Redid everything

--- app.py
+++ app.py
@@ -2,21 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
-def index():
-    return render_template('index.html')
+# Your individual route handlers are removed
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+# This route will handle the dropdown menu rendering based on a parameter
+@app.route('/<selected_item>')
+def dropdown(selected_item):
+    return render_template(f'{selected_item}.html')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-@app.route('/forgot')
-def forgot():
-    return render_template('forgot.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
