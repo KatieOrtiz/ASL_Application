1f87bab145ec4b11e218b754fadd599a1df8f83b
Katie Ortiz <katrina.ortiz.673@my.csun.edu>
2023-11-13 23:47:30 -0800

    Added content and style

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Your individual route handlers are removed
 
 # This route will handle the dropdown menu rendering based on a parameter
 @app.route('/<selected_item>')
